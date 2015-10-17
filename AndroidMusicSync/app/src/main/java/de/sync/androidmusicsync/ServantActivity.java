748090795d0c89defd169467fc38397b05c857e1
Lukas Krause <esuarksakul@t-online.de>
2015-10-17 22:30:50 +0200

    Einfügen des Servants.

--- AndroidMusicSync/app/src/main/java/de/sync/androidmusicsync/ServantActivity.java
+++ AndroidMusicSync/app/src/main/java/de/sync/androidmusicsync/ServantActivity.java
@@ -2,6 +2,13 @@ package de.sync.androidmusicsync;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 
 public class ServantActivity extends AppCompatActivity {
 
@@ -10,9 +17,31 @@ public class ServantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servant);
 
-        /*TODO
-            Den Diener einbinden.
+        checkMicrophone();
+    }
 
-         */
+    private void checkMicrophone() {
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(24000, 1024, 0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult result,AudioEvent e) {
+                final float pitchInHz = result.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (pitchInHz != -1) {
+                            System.out.println(pitchInHz);
+                        }
+                        if (pitchInHz <= 18500 && pitchInHz >= 17500) {
+                            System.err.println("Pitch Richtig");
+                        }
+                    }
+                });
+            }
+        };
+        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 24000, 1024, pdh);
+        dispatcher.addAudioProcessor(p);
+        new Thread(dispatcher,"Audio Dispatcher").start();
     }
 }
