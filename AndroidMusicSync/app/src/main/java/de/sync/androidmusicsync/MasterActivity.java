f987a047c1b96922b9a7b593377e459b2f267f8f
d0cod3r <nic.es@gmx.de>
2015-10-17 20:18:56 +0200

    erste Version mit Design

---
+++ AndroidMusicSync/app/src/main/java/de/sync/androidmusicsync/MasterActivity.java
@@ -0,0 +1,20 @@
+package de.sync.androidmusicsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MasterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_master);
+    }
+    public void masterEvent(View view){
+        /*TODO
+        * Hier muss der Master integriert werden.
+        *
+        */
+    }
+}
---
+++ AndroidMusicSync/app/src/main/java/de/sync/androidmusicsync/ServantActivity.java
@@ -0,0 +1,18 @@
+package de.sync.androidmusicsync;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ServantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servant);
+
+        /*TODO
+            Den Diener einbinden.
+
+         */
+    }
+}
